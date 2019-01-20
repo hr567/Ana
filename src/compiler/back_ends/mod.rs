b12045bc2a955d7e63d7760d2d1355f37f332383
hr567 <hr567@hr567.me>
2019-01-20 22:09:08 +0800

    Use compiler features

--- src/compiler/back_ends/mod.rs
+++ src/compiler/back_ends/mod.rs
@@ -1,7 +1,62 @@
 use super::*;
 
+#[cfg(feature = "gcc")]
 mod c_gcc;
-pub use c_gcc::CGcc;
-
+#[cfg(feature = "gxx")]
 mod cpp_gxx;
-pub use cpp_gxx::CppGxx;
+
+pub fn get_compiler(language: &str) -> result::Result<Box<dyn Compiler>, &'static str> {
+    match language {
+        #[cfg(feature = "gcc")]
+        "c.gcc" => Ok(Box::new(c_gcc::CGcc::new())),
+        #[cfg(feature = "gxx")]
+        "cpp.gxx" => Ok(Box::new(cpp_gxx::CppGxx::new())),
+        _ => Err("Language or compiler is not support"),
+    }
+}
+
+fn rename_with_new_extension(
+    origin_file: &path::Path,
+    new_extension: &str,
+) -> io::Result<Box<path::Path>> {
+    let mut new_file = origin_file.to_path_buf();
+    new_file.set_extension(new_extension);
+    fs::rename(&origin_file, &new_file)?;
+    Ok(new_file.into_boxed_path())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::process;
+
+    use tempfile;
+
+    #[cfg(feature = "gcc")]
+    #[test]
+    fn test_c_language_compile() -> io::Result<()> {
+        let work_dir = tempfile::tempdir()?;
+        let executable_file = work_dir.path().join("c_compile_test.exe");
+        let source_file = work_dir.path().join("c_compile_test.c");
+        fs::write(&source_file, "#include<stdio.h>\nint main() { return 0; }")
+            .expect("Failed to write source code");
+        let compiler = get_compiler("c.gcc").unwrap();
+        assert!(compiler.compile(&source_file, &executable_file)?.is_ok());
+        assert!(process::Command::new(&executable_file).status()?.success());
+        Ok(())
+    }
+
+    #[cfg(feature = "gxx")]
+    #[test]
+    fn test_cpp_language_compile() -> io::Result<()> {
+        let work_dir = tempfile::tempdir()?;
+        let executable_file = work_dir.path().join("cpp_compile_test.exe");
+        let source_file = work_dir.path().join("cpp_compile_test.cpp");
+        fs::write(&source_file, "#include<iostream>\nint main() { return 0; }")
+            .expect("Failed to write source code");
+        let compiler = get_compiler("cpp.gxx").unwrap();
+        assert!(compiler.compile(&source_file, &executable_file)?.is_ok());
+        assert!(process::Command::new(&executable_file).status()?.success());
+        Ok(())
+    }
+}
--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -4,7 +4,7 @@ use std::path;
 use std::result;
 
 mod back_ends;
-use back_ends::{CGcc, CppGxx};
+use back_ends::get_compiler;
 
 pub type Result = result::Result<(), String>;
 
@@ -20,60 +20,8 @@ pub fn compile(
     }
 }
 
-trait Compiler {
+pub trait Compiler {
     fn suffix(&self) -> &'static str;
     fn compile(&self, source_file: &path::Path, executable_file: &path::Path)
         -> io::Result<Result>;
-}
-
-fn get_compiler(language: &str) -> result::Result<Box<dyn Compiler>, &'static str> {
-    match language {
-        "c.gcc" => Ok(Box::new(CGcc::new())),
-        "cpp.gxx" => Ok(Box::new(CppGxx::new())),
-        _ => Err("Language or compiler is not support"),
-    }
-}
-
-fn rename_with_new_extension(
-    origin_file: &path::Path,
-    new_extension: &str,
-) -> io::Result<Box<path::Path>> {
-    let mut new_file = origin_file.to_path_buf();
-    new_file.set_extension(new_extension);
-    fs::rename(&origin_file, &new_file)?;
-    Ok(new_file.into_boxed_path())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::process;
-
-    use tempfile;
-
-    #[test]
-    fn test_c_language_compile() -> io::Result<()> {
-        let work_dir = tempfile::tempdir()?;
-        let executable_file = work_dir.path().join("c_compile_test.exe");
-        let source_file = work_dir.path().join("c_compile_test.c");
-        fs::write(&source_file, "#include<stdio.h>\nint main() { return 0; }")
-            .expect("Failed to write source code");
-        let compiler = get_compiler("c.gcc").unwrap();
-        assert!(compiler.compile(&source_file, &executable_file)?.is_ok());
-        assert!(process::Command::new(&executable_file).status()?.success());
-        Ok(())
-    }
-
-    #[test]
-    fn test_cpp_language_compile() -> io::Result<()> {
-        let work_dir = tempfile::tempdir()?;
-        let executable_file = work_dir.path().join("cpp_compile_test.exe");
-        let source_file = work_dir.path().join("cpp_compile_test.cpp");
-        fs::write(&source_file, "#include<iostream>\nint main() { return 0; }")
-            .expect("Failed to write source code");
-        let compiler = get_compiler("cpp.gxx").unwrap();
-        assert!(compiler.compile(&source_file, &executable_file)?.is_ok());
-        assert!(process::Command::new(&executable_file).status()?.success());
-        Ok(())
-    }
 }
