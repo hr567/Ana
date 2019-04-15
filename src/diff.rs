40227ef0d7331eafa5ff2199dbd6fd07f3fc7930
hr567 <hr567@hr567.me>
2019-04-15 13:48:11 +0800

    Compare output and answer line by line

--- src/diff.rs
+++ src/diff.rs
@@ -1,32 +1,68 @@
 use std::fs;
-use std::io;
+use std::io::{self, BufRead};
 use std::path;
 use std::process;
-use std::str;
 
-fn diff(output: &[u8], answer: &[u8]) -> bool {
-    let output = match str::from_utf8(&output) {
-        Ok(res) => res,
-        _ => return false,
-    };
-    let answer = match str::from_utf8(&answer) {
-        Ok(res) => res,
-        _ => return false,
-    };
-    !output
-        .trim_end_matches('\n')
-        .split_terminator('\n')
-        .zip(answer.trim_end_matches('\n').split_terminator('\n'))
-        .all(|(output, answer)| output.trim_end_matches(' ') == answer.trim_end_matches(' '))
+/// Check if two buffers is equal
+///
+/// Ignore the empty line at end of file and space at end of line.
+/// Return `false` if there is no difference between two buffer
+fn buf_diff<T: BufRead, U: BufRead>(output_buf: &mut T, answer_buf: &mut U) -> io::Result<bool> {
+    loop {
+        let (output, output_eof) = {
+            let mut res = Vec::new();
+            let eof = output_buf.read_until(b'\n', &mut res)? == 0;
+            while res.ends_with(&[b' ']) || res.ends_with(&[b'\n']) {
+                res.pop();
+            }
+            (res, eof)
+        };
+
+        let (answer, answer_eof) = {
+            let mut res = Vec::new();
+            let eof = answer_buf.read_until(b'\n', &mut res)? == 0;
+            while res.ends_with(&[b' ']) || res.ends_with(&[b'\n']) {
+                res.pop();
+            }
+            (res, eof)
+        };
+
+        if output_eof && answer_eof {
+            break;
+        }
+
+        if output != answer {
+            return Ok(true);
+        }
+    }
+
+    Ok(false)
 }
 
+/// Compare output file and answer file line by line
+///
+/// Ignore the blank line at the end of file
+/// and white space at the end of line.
+/// Return `true` if there is no difference between two files
+///
+/// Return `Err` when appear any IO error
 pub fn check(output_file: &path::Path, answer_file: &path::Path) -> io::Result<bool> {
-    Ok(!diff(
-        fs::read(&output_file)?.as_slice(),
-        fs::read(&answer_file)?.as_slice(),
-    ))
+    let mut output_buf = {
+        let file = fs::File::open(output_file)?;
+        io::BufReader::new(file)
+    };
+
+    let mut answer_buf = {
+        let file = fs::File::open(answer_file)?;
+        io::BufReader::new(file)
+    };
+
+    Ok(!buf_diff(&mut output_buf, &mut answer_buf)?)
 }
 
+/// Check output by provided spj program
+///
+/// Return `true` if spj program exit successfully
 pub fn check_with_spj(
     input_file: &path::Path,
     output_file: &path::Path,
@@ -47,6 +83,10 @@ mod tests {
 
     use tempfile;
 
+    fn diff(output: &[u8], answer: &[u8]) -> bool {
+        buf_diff(&mut io::Cursor::new(output), &mut io::Cursor::new(answer)).unwrap()
+    }
+
     #[test]
     fn test_diff_complete_eq() {
         assert!(!diff(b"hello world", b"hello world"));
@@ -55,13 +95,19 @@ mod tests {
     #[test]
     fn test_diff_with_empty_line_at_eof() {
         assert!(!diff(b"hello world\n", b"hello world"));
+        assert!(!diff(b"hello world\n\n", b"hello world"));
+        assert!(!diff(b"hello world\n", b"hello world\n"));
+        assert!(!diff(b"hello world\n\n", b"hello world\n"));
+        assert!(!diff(b"hello world\n\n", b"hello world\n\n"));
         assert!(!diff(b"hello world", b"hello world\n"));
+        assert!(!diff(b"hello world", b"hello world\n\n"));
     }
 
     #[test]
     fn test_diff_with_space_at_eol() {
         assert!(!diff(b"hello world ", b"hello world"));
-        assert!(!diff(b"hello world", b"hello world "));
+        assert!(!diff(b"hello world  ", b"hello world"));
+        assert!(!diff(b"hello world", b"hello world  "));
     }
 
     #[test]
@@ -74,6 +120,8 @@ mod tests {
         assert!(!diff(b"hello world\n", b"hello world \n"));
         assert!(!diff(b"hello world\n ", b"hello world\n"));
         assert!(!diff(b"hello world\n", b"hello world\n "));
+        assert!(!diff(b"hello world\n \n", b"hello world\n"));
+        assert!(!diff(b"hello world\n", b"hello world\n \n"));
     }
 
     #[test]
@@ -89,10 +137,4 @@ mod tests {
         assert!(!check(&file0, &file2)?);
         Ok(())
     }
-
-    // TODO: Add Spj test
-    // #[test]
-    // fn test_check_with_spj() -> io::Result<()> {
-    //     unimplemented!()
-    // }
 }
